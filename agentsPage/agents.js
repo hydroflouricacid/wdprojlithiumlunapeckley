0d879e3d5558b3df8c19a4cc7835ed14813415bd
hydroflouricacid <peckley.araw@gmail.com>
2025-01-30 16:11:09 +0800

    WHYYYYYY

--- agentsPage/agents.js
+++ agentsPage/agents.js
@@ -1,13 +1,15 @@
 function changeContent(evt, agentName) {
   // Declare all variables
-  var i, tabcontent, tablinks;
+  var tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("agentDescription");
-  for (i = 0; i < tabcontent.length; i++) {
+  for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+    console.log("AUGUUGAUIHIDS");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(agentName).style.display = "block";
+  var activetab = document.getElementById(agentName);
+  activetab.style.display = "none";
+  console.log("AISAHIUDABUISD");
 } 
